da78760a9eea5cfddeb0dbe4cb121cf1c775cd0e
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-15 16:54:21 +0530

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,19 @@
+# wapp using __ and captial and small letters
+
+If = 10
+anD = 20
+OR = If + anD
+print(OR)
+
+n1 = 30
+n2 = 40
+r3 = n1 - n2
+print(r3)
+
+_ = 5
+__ = 6
+___= _ + __
+print(___)
+
+# this is just a fun try
+# we wrote some letters in capital and small of if,and , are because they are functions by themselves
